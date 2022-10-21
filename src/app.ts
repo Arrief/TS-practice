694d2edb39bd14e6f26e66b3b7f8a3a78e0dd09c
Arne Rief <arne.rief@gmail.com>
2022-10-21 23:31:59 +0200

    functionality to get & validate user input

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,77 @@
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  // check length of string
+  minLength?: number;
+  maxLength?: number;
+  // check if number is above or below a certain range
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  // default assumption
+  let isValid = true;
+  if (validatableInput.required) {
+    // if required is set but validatableInput is empty, the short circuiting will set isValid to false
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  /* We want to check for null/undefined instead of just "if(validatableInput.minLength)" - minLength could theoretically be 0, which would result in "false"; also !="" with only one "=" means JS checks both if it's null OR undefined without us having to specify that */
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length > validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length < validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value > validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value < validatableInput.max;
+  }
+  return isValid;
+}
+
+// autobind decorator
+function autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFN = originalMethod.bind(this);
+      return boundFN;
+    },
+  };
+  return adjustedDescriptor;
+}
+
 // class to render the input form in the HTML
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     // type-casting necessary to let TS know that the result of getElementById will be of the appropriate type
@@ -16,7 +85,85 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    // input elements
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  // returns either a tuple or nothing in case of invalid input
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    // "value" give us a string, convert below in return to number!
+    const enteredPeople = this.peopleInputElement.value;
+
+    // objects to check if user input meets validation criteria
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    // validation all user input
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert("Invalid input, please try again!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  // clear the input fields after submit
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    // Array object comes with method to check if argument is an array or not and out tuple is basically an array
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
+  }
+
+  private configure() {
+    // simple alternative w/o autobind decorator:
+    // this.element.addEventListener("submit", this.submitHandler.bind(this));
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
